16f41a3f2803830e5740bce3ee48943f1cf2fa5c
herby raynaud <herby@yieldmo.com>
2022-06-02 20:36:17 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue';
+
+import MainLayout from './layouts/MainLayout.vue';
+import LoginPage from './pages/LoginPage.vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
