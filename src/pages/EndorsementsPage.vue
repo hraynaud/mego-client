101e4678941b4aa1ca160b8a0d68cccdcc58e6b1
hraynaud <herby.raynaud@gmail.com>
2022-09-18 21:30:00 -0400

    WIP search form

--- src/pages/EndorsementsPage.vue
+++ src/pages/EndorsementsPage.vue
@@ -2,6 +2,35 @@
   <q-page>
     <div class="q-pa-md">
       <h3>Endorsements</h3>
+
+      <q-form @submit.prevent="onSubmit">
+        <div class="row items-baseline justify-start q-gutter-lg">
+          <label>Filter:</label>
+          <div class="col-2 search-filter">
+            <q-select
+              v-model="friend"
+              single
+              :options="friends"
+              :option-value="(opt) => opt?.id"
+              :option-label="(opt) => opt?.attributes?.firstName"
+              use-chips
+              label="Friends"
+            />
+          </div>
+          <div class="col-4 search-filter">
+            <q-select
+              v-model="topic"
+              single
+              :options="topics"
+              :option-value="(opt) => opt?.id"
+              :option-label="(opt) => opt?.attributes?.name"
+              use-chips
+              label="Topics"
+            />
+          </div>
+          <q-btn round color="primary" icon="search" type="submit" />
+        </div>
+      </q-form>
     </div>
     <div class="q-pa-md">
       <h5>Search Results</h5>
@@ -34,11 +63,14 @@
         </q-card>
       </div>
     </div>
+    <q-page-sticky position="bottom-right" :offset="[64, 36]">
+      <q-btn fab icon="add" color="accent" to="/endorsements/new" />
+    </q-page-sticky>
   </q-page>
 </template>
 
 <script setup>
-import { apiService, endorsementService } from '../core/services';
+import { endorsementService } from '../core/services';
 import { jsonResponseHandler } from '../core/services';
 import { ref, onMounted } from 'vue';
 
