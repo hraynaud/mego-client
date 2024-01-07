d0a517812689888c8cf2a043f4628870f69f53b4
hraynaud <herby.raynaud@gmail.com>
2024-01-07 13:35:34 -0500

    updates profile page endorsement cards

--- src/components/EndorsementCard.vue
+++ src/components/EndorsementCard.vue
@@ -1,31 +1,35 @@
 <template>
   <q-card class="endorsement-card">
-    <q-item>
-      <q-item-section avatar>
-        <q-avatar>
+    <q-card-section avatar>
+      <div class="endorsement-topic text-h6 text-center q-mb-xs">
+        {{ e?.topic }}
+      </div>
+      <div class="text-center endorsee-hdr">
+        <q-avatar class="avatar" v-if="e?.endorseeAvatarUrl != 'anonymous.png'">
           <img :src="avatar()" />
         </q-avatar>
-      </q-item-section>
+        <q-avatar v-else icon="psychology" class="avatar anon profile">
+        </q-avatar>
+      </div>
 
-      <q-item-section>
-        <q-item-label>{{ e?.topic }}</q-item-label>
-      </q-item-section>
-    </q-item>
+      <div class="q-pa-sm endorsement-description">
+        {{ e?.description }}
+      </div>
+      <q-separator />
+      <q-card-section class="row justify-end">
+        <q-btn
+          v-if="deleteable"
+          class="col-3"
+          size="12px"
+          flat
+          dense
+          round
+          icon="delete"
+          @click="deleteMe(e!, idx!)"
+        />
+      </q-card-section>
+    </q-card-section>
 
-    <!-- <img :src="`/images/${e.topicImage}`" /> -->
-
-    <q-card-section> {{ e?.description }} </q-card-section>
-
-    <q-btn
-      v-if="deleteable"
-      class="gt-xs float-right"
-      size="12px"
-      flat
-      dense
-      round
-      icon="delete"
-      @click="deleteMe(e!, idx!)"
-    />
     <!-- @click.prevent.once="bus.emit('delete-endorsement', e, idx)" -->
   </q-card>
 </template>
@@ -53,9 +57,57 @@ const deleteMe = (e: EndorsementModel, i: number) => {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .endorsement-card {
-  width: 30%;
-  // max-width: 250px;
+  width: 200px;
+  height: 225px;
+  padding: 0px;
+  border-radius: 8px;
+}
+
+.endorsement-card .q-card__section--vert {
+  padding: 0px;
+}
+
+.endorsement-topic {
+  font-weight: 100;
+  background-color: $secondary;
+  padding-top: 4%;
+  padding-bottom: 10%;
+  color: white;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  height: 65px;
+}
+
+.endorsee-hdr {
+  margin-top: -1.95rem;
+
+  .avatar {
+    width: 60px;
+    height: 60px;
+    margin-bottom: 2%;
+    margin-top: 1%;
+  }
+}
+
+.endorsement-description {
+  height: 85px;
+}
+
+.endorsement-avatar {
+  width: 60px;
+  height: 60px;
+
+  .endorsee {
+    border-color: $accent;
+  }
+}
+
+.anon {
+  background-color: $tertiary;
+  color: darkslategrey;
+  font-weight: bold;
+  font-size: 3em;
 }
 </style>
