752ca37013d5eb658392c84f5e3ffea05b87911d
hraynaud <herby.raynaud@gmail.com>
2024-01-28 13:11:41 -0500

    wip homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,68 +1,74 @@
 <template>
-  <div><H3>Recent Activity</H3></div>
-  <div>
-    <q-page>
-      <div class="q-pa-md">
-        <h5>New Projects</h5>
-        <div class="q-gutter-md row items-start">
-          <ProjectList :projects="projects"></ProjectList>
+  <q-page>
+    <page-header>Dashboard</page-header>
+
+    <!-- <div class="flex flex-center column">
+      <div class="row" style="min-height: 400px; width: 100%; padding: 16px">
+        <div
+          id="parent"
+          class="fit row wrap justify-around"
+          style="overflow: hidden"
+        >
+          <div class="col-3 bg-grey-6" style="overflow: auto">
+            <div class="q-pa-md">
+              <person-card :p="me" />
+            </div>
+          </div>
         </div>
       </div>
-      <q-page-sticky position="bottom-right" :offset="[64, 36]">
-        <q-btn fab icon="add" color="accent" to="/endorsements/new" />
-      </q-page-sticky>
-    </q-page>
-
-    <div class="q-pa-md q-gutter-md">
-      <q-list bordered class="rounded-borders" style="max-width: 350px">
-        <q-item-label header>Friends</q-item-label>
-
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-avatar>
-              <img src="" />
-            </q-avatar>
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label lines="1">Brunch this weekend?</q-item-label>
-            <q-item-label caption lines="2">
-              <span class="text-weight-bold">Janet</span>
-              -- I'll be in your neighborhood doing errands this weekend. Do you
-              want to grab brunch?
-            </q-item-label>
-          </q-item-section>
-
-          <q-item-section side top> 1 min ago </q-item-section>
-        </q-item>
-
-        <q-separator inset="item" />
-
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/img/avatar4.jpg" />
-            </q-avatar>
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label lines="1">Linear Project</q-item-label>
-            <q-item-label caption lines="2">
-              <span class="text-weight-bold">John</span>
-              -- Can we schedule a call for tomorrow?
-            </q-item-label>
-          </q-item-section>
-
-          <q-item-section side top> 1 min ago </q-item-section>
-        </q-item>
-      </q-list>
+    </div> -->
+    <div class="flex flex-center column">
+      <div
+        class="row bg-blue-grey-2"
+        style="min-height: 400px; width: 95%; padding: 24px"
+      >
+        <div
+          id="parent"
+          class="fit row wrap justify-around"
+          style="overflow: hidden"
+        >
+          <div class="col-3 bg-grey-6" style="overflow: auto">
+            <person-card :p="me" />
+          </div>
+          <div class="col-6 bg-grey-6" style="overflow: auto">
+            <q-card class="no-border-radius">
+              <q-card-section> Child #2 </q-card-section>
+            </q-card>
+          </div>
+          <div class="col-2 bg-grey-6" style="overflow: auto">
+            <q-card class="no-border-radius">
+              <q-card-section> Child #3 </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+  </q-page>
 </template>
+<script setup lang="ts">
+import PageHeader from 'src/components/PageHeader.vue';
+import PersonCard from 'src/components/PersonCard.vue';
+// import ProjectList from '../components/ProjectList.vue';
+// import { useProjectList } from 'src/composables/use-project-list';
+import { peopleApi, peopleService, authService } from '../core/services';
+import { PersonModel } from 'src/core/models';
+import { ref, onBeforeMount, onMounted } from 'vue';
 
-<script setup>
-import ProjectList from '../components/ProjectList.vue';
-import { useProjectList } from 'src/composables/use-project-list';
+const me = ref({} as PersonModel);
+//TODO const me = ref<PersonModel>() understand why this doesn't work.
 
-const { projects } = useProjectList();
+const loadme = () => {
+  const id = authService.currentUser()['uid'];
+
+  peopleApi.findPerson(id).then(function (resp: { data: any }) {
+    me.value = peopleService.buildPerson(resp.data.data);
+    // debugger;
+  });
+};
+
+onBeforeMount(() => {
+  loadme();
+});
+
+// const { projects } = useProjectList();
 </script>
