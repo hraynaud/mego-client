6dd9c9af804e39be793fd60287fb725e5d4c4eb9
hraynaud <herby.raynaud@gmail.com>
2025-01-20 18:57:23 -0500

    convert endorsement search to AI

--- src/pages/EndorsementsPage.vue
+++ src/pages/EndorsementsPage.vue
@@ -1,43 +1,87 @@
 <template>
   <q-page>
-    <page-header>Endorsements</page-header>
+    <page-header>Explore your network</page-header>
+    <h5 class="text-center">How can I help you today?</h5>
+    <div class="q-pa-xl">
+      <q-input outlined v-model="query" placeholder="Ask Mego...">
+        <template v-slot:append>
+          <q-icon name="search" class="cursor-pointer" @click="gotopage" />
+        </template>
+      </q-input>
+    </div>
 
     <div class="q-pa-md">
       <EndorsementPathList :endorsements="endorsements" />
     </div>
     <div class="q-pa-lg flex flex-center">
-      <q-pagination
-        v-model="current"
-        :max="10"
-        :boundary-numbers="false"
-        @update:model-value="gotopage"
-      />
+      <q-pagination v-model="current" :max="10" :boundary-numbers="false" />
     </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { useEndorsementsList } from 'src/composables/use-endorsement-list';
+// import {
+//   useEndorsementsList,
+// } from 'src/composables/use-endorsement-list';
+import { ComputedRef, computed, ref } from 'vue';
+import { useQuasar } from 'quasar';
 import EndorsementPathList from '../components/EndorsementPathList.vue';
-
 import PageHeader from '../components/PageHeader.vue';
-import { Ref, ref } from 'vue';
+import {
+  SearchParams,
+  useEndorsementStore,
+} from '../stores/endorsements-store';
 import { EndorsementPathModel } from 'src/core/models';
+const $q = useQuasar();
 const current = ref(1);
-const query = ref('Who do I know that knows haute couture');
-const topicName = ref('Design');
-import { SearchParams } from '../stores/endorsements-store';
-let endorsements: Ref<EndorsementPathModel[]> = ref([]);
+const query = ref();
+const topicName = ref();
 const seachParams = ref<SearchParams>({
   page: undefined,
   query: undefined,
   topicName: undefined,
 });
+const eStore = useEndorsementStore();
+const endorsements: ComputedRef<EndorsementPathModel[]> = computed(
+  () => eStore.endorsements
+);
+
+const unsubscribe = eStore.$onAction(
+  ({
+    name, // name of the action
+    store, // store instance, same as `someStore`
+    args, // array of parameters passed to the action
+    after, // hook after the action returns or resolves
+    onError, // hook if the action throws or rejects
+  }) => {
+    const startTime = Date.now();
+    if (name == 'loadEndorsements') {
+      $q.loading.show({
+        delay: 0, // ms
+      });
+    }
+
+    after((result) => {
+      if (name == 'setEndorsements') {
+        $q.loading.hide();
+      }
+    });
+
+    // this will trigger if the action throws or returns a promise that rejects
+    onError((error) => {
+      console.warn(
+        `Failed "${name}" after ${Date.now() - startTime}ms.\nError: ${error}.`
+      );
+    });
+  }
+);
+
 const gotopage = () => {
-  seachParams.value.page = current.value;
-  seachParams.value.query = query.value;
-  seachParams.value.topicName = topicName.value;
-  endorsements = useEndorsementsList(seachParams.value);
+  if (query.value !== undefined) {
+    seachParams.value.page = current.value;
+    seachParams.value.query = query.value;
+    seachParams.value.topicName = topicName.value;
+    eStore.loadEndorsements(seachParams.value, current.value);
+  }
 };
-gotopage();
 </script>
--- src/stores/endorsements-store.ts
+++ src/stores/endorsements-store.ts
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia';
 import { EndorsementPathModel } from 'src/core/models';
+import { endorsementService } from 'src/core/services';
 
 interface EndorsementState {
   endorsementList: Map<number, EndorsementPathModel[]>;
@@ -25,17 +26,43 @@ export const useEndorsementStore = defineStore('endorsements', {
   }),
   getters: {
     endorsements: (state) => {
-      return (page: number) => {
-        return state.endorsementList.get(page);
-      };
+      // return (page: number) => {
+      return state.endorsementList.get(1) ?? [];
+      // };
     },
     findById: (state) => {
+      //TODO restore endorsement by id to laod endorsement view page.
       return (id: string) => {
         // state.endorsementList.find((endorsement) => endorsement.id == id);
       };
     },
   },
   actions: {
+    loadEndorsements(params: SearchParams, page: number) {
+      endorsementService
+        .search(params)
+        .then((res: any) => {
+          if (res) {
+            const endorsements: EndorsementPathModel[] = [];
+            res.data.data.map((e: any) => {
+              endorsements.push(
+                new EndorsementPathModel(
+                  e.id,
+                  e.attributes.topic,
+                  e.attributes.description,
+                  e.attributes.endorseeProfileImageUrl,
+                  e.attributes.endorseeName,
+                  e.attributes.path
+                )
+              );
+            });
+            this.setEndorsements(page, endorsements);
+          }
+        })
+        .catch((error: any): void => {
+          console.log(error);
+        });
+    },
     setEndorsements(page: number, endorsements: EndorsementPathModel[]) {
       if (endorsements.length == 0) return;
       this.endorsementList.set(page, endorsements);
