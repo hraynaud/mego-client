b8647160aa1df6341c9a7b2cb849b116252125b5
hraynaud <herby.raynaud@gmail.com>
2025-01-20 18:37:05 -0500

    adds loading plugin

--- quasar.config.js
+++ quasar.config.js
@@ -90,7 +90,7 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
-      config: {},
+      config: { loading: { message: 'searching your network please wait' } },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -103,7 +103,7 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['Notify', 'Dialog'],
+      plugins: ['Notify', 'Dialog', 'Loading'],
     },
 
     // animations: 'all', // --- includes all animations
