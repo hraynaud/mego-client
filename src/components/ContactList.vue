ed257ec1399bce713398a3ae87159f73a6d60b38
hraynaud <herby.raynaud@gmail.com>
2025-01-20 18:38:23 -0500

    wraps user in router link for navigation

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="q-gutter-md row">
-    <person-card v-for="p in people" :p="p" :key="p.firstName" />
+    <div v-for="p in people" :p="p" :key="p.firstName">
+      <router-link :to="`/person/${p.id}`">
+        <person-card :p="p" />
+      </router-link>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 import { PersonModel } from '../core/models';
 import PersonCard from '../components/PersonCard.vue';
+import { RouterLink } from 'vue-router';
 
 defineProps<{ people: PersonModel[] }>();
 </script>
