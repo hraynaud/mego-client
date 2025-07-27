58eb30c8abd0047ce58747f027e52469a8e675b6
hraynaud <herby.raynaud@gmail.com>
2025-07-27 10:02:46 -0400

    udpates node

--- src/pages/SignupPage.vue
+++ src/pages/SignupPage.vue
@@ -1,102 +1,48 @@
 <template>
-  <q-page
-    class="window-height window-width row justify-center items-center"
-    style="background: linear-gradient(#8274c5, #5a4a9f)"
-  >
-    <q-card square class="shadow-24" style="width: 300px">
-      <q-card-section class="bg-deep-purple-7">
-        <h4 class="text-h5 text-white q-my-md">Sign Up</h4>
-      </q-card-section>
-      <q-form @submit="onSubmit" @reset="onReset">
-        <q-card-section class="q-px-sm q-pt-m q-pb-l">
-          <q-input
-            square
-            clearable
-            v-model="firstName"
-            type="text"
-            label="First Name"
-          >
-            <template v-slot:prepend>
-              <q-icon name="name" />
-            </template>
-          </q-input>
-          <q-input
-            square
-            clearable
-            v-model="lastName"
-            type="text"
-            label="Last Name"
-          >
-            <template v-slot:prepend>
-              <q-icon name="name" />
-            </template>
-          </q-input>
-          <q-input square clearable v-model="email" type="email" label="Email">
-            <template v-slot:prepend>
-              <q-icon name="email" />
-            </template>
-          </q-input>
-          <q-input
-            square
-            clearable
-            v-model="password"
-            type="password"
-            label="Password"
-          >
-            <template v-slot:prepend>
-              <q-icon name="lock" />
-            </template>
-          </q-input>
-          <q-input
-            square
-            clearable
-            v-model="passwordConfirmation"
-            type="password"
-            label="Password Confirmation"
-          >
-            <template v-slot:prepend>
-              <q-icon name="lock" />
-            </template>
-          </q-input>
+  <q-page class="window-height window-width row justify-center items-center">
+    <div style="background-image: url('../../src/assets/login/bg-2.png'); width: 100%; height: inherit;display: grid;">
+      <div class="q-my-lg" style="width: 300px;  text-align: center; place-self: center;">
+        <q-form @submit="onSubmit" @reset="onReset" class="q-px-sm q-pt-m q-pb-lg">
+          <q-card class="q-py-lg">
 
-          <q-input
-            square
-            clearable
-            v-model="inviteCode"
-            type="text"
-            readonly
-            label="Invite Code"
-            v-if="inviteCode"
-          >
-            <template v-slot:prepend>
-              <q-icon name="email" />
-            </template>
-          </q-input>
-        </q-card-section>
+            <q-card-section class="q-mb-sm">
+              <div>Create an account:</div>
+              <q-input outlined dense clearable v-model="email" type="email" label="Email" class="q-mb-md" />
+              <q-input outlined dense clearable v-model="password" type="password" label="Password" class="q-mb-md" />
+              <q-input outlined dense clearable v-model="passwordConfirmation" type="password"
+                label="Password Confirmation" />
 
-        <q-card-actions class="q-px-lg">
-          <q-btn
-            unelevated
-            size="lg"
-            color="purple-4"
-            class="full-width text-white"
-            label="Get Started"
-            type="submit"
-          />
-          <q-card-section class="text-center q-pa-sm">
-            <router-link class="text-grey-6" to="signup">Login</router-link>
-          </q-card-section>
-          <q-card-section class="text-center q-pa-sm">
-            <router-link class="text-grey-6" to="signup"
-              >Forgot Password?</router-link
-            >
-          </q-card-section>
-        </q-card-actions>
-      </q-form>
-      <!-- <q-card-section class="text-center q-pa-sm">
-        <p class="text-grey-6">Return to login</p>
-      </q-card-section> -->
-    </q-card>
+              <q-input square clearable v-model="inviteCode" type="text" readonly label="Invite Code"
+                v-if="inviteCode" />
+
+            </q-card-section>
+            <div class="divider">
+              <span>OR</span>
+            </div>
+            <q-card-section>
+              <q-btn class="q-my-xs full-width" color="white" no-caps outline text-color="black" align="center">
+                <q-icon name="img:https://hr-test-knowbuddyz.s3.us-east-1.amazonaws.com/Google.svg" left
+                  size="16px"></q-icon>
+                <div class="text-weight-light">Continue with Google</div>
+              </q-btn>
+
+            </q-card-section>
+
+
+
+            <q-card-section class="">
+              <q-btn color="black" class="full-width" :ripple="false" label="Discover your community!" type="submit"
+                no-caps />
+            </q-card-section>
+
+            <span class="text-weight-light">
+              Already a member?
+              <router-link class="text-weight-regular" to="/auth/login">Login
+              </router-link></span>
+          </q-card>
+        </q-form>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -111,14 +57,10 @@ import { useRouter, useRoute } from 'vue-router';
 const $q = useQuasar();
 const router = useRouter();
 const curRoute = useRoute();
-
-const firstName = ref<string>('');
-const lastName = ref<string>('');
 const email = ref<string>('');
 const password = ref<string>('');
 const passwordConfirmation = ref<string>('');
 const inviteCode = ref<string | undefined>(undefined);
-let loading = false;
 let error = '';
 
 onMounted(() => {
@@ -126,17 +68,14 @@ onMounted(() => {
 });
 
 function onSubmit() {
-  loading = true;
+
   authService
     .signup(
       email.value,
       password.value,
-      firstName.value,
-      lastName.value,
       inviteCode.value
     )
     .then(() => {
-      loading = false;
       router.replace('/');
     })
     .catch((err) => {
@@ -152,8 +91,6 @@ function onSubmit() {
 }
 
 function onReset() {
-  firstName.value = '';
-  lastName.value = '';
   email.value = '';
   password.value = '';
   passwordConfirmation.value = '';
